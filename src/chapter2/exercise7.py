4564d4ed4f2cb816a61520c08a3608d91ec52034
Your Name <you@example.com>
2021-03-26 13:05:41 +0300

    8.0 Commit the changes onto the fork for making

--- src/chapter2/exercise7.py
+++ src/chapter2/exercise7.py
@@ -2,20 +2,20 @@ dolla = float(input("Enter amount to take charge for: "))
 
 print("Your change is: ")
 
-twenties = int(dolla // 20)
-ten = int((dolla % 20) // 10)
-fives = int(((dolla % 20) % 10)//5)
-ones = int(((dolla % 20) % 10) // 1)
-quarter = int((((dolla % 20) % 10) % 1) // 0.25)
-dime = int(((((dolla % 20) % 10) % 1) % 0.25) // 0.1)
-nickel = int((((((dolla % 20) % 10) % 1) % 0.25) % 0.1) // 0.05)
-penny = int(((((((dolla % 20) % 10) % 1) % 0.25) % 0.1) % 0.05)//0.01)
+twenties = dolla // 20
+ten = (dolla % 20) // 10
+fives = ((dolla % 20) % 10)//5
+ones = (((dolla % 20) % 10) % 5) // 1
+quarter = ((((dolla % 20) % 10) % 5) % 1) // 0.25
+dime = (((((dolla % 20) % 10) % 5) % 1) % 0.25) // 0.1
+nickel = ((((((dolla % 20) % 10) % 5) % 1) % 0.25) % 0.1) // 0.05
+penny = (((((((dolla % 20) % 10) % 5) % 1) % 0.25) % 0.1) % 0.05)//0.01
 
-print(f'{twenties} twenties')
-print(f'{ten} ten')
-print(f'{fives} five')
-print(f'{ones} ones')
-print(f'{quarter} quarters')
-print(f'{dime} dimes')
-print(f'{nickel} nickel')
-print(f'{penny} penny')
+print(f'{int(twenties)} twenties')
+print(f'{int(ten)} ten')
+print(f'{int(fives)} five')
+print(f'{int(ones)} ones')
+print(f'{int(quarter)} quarters')
+print(f'{int(dime)} dimes')
+print(f'{int(nickel)} nickels')
+print(f'{int(penny)} pennies')
