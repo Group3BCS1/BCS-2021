2d6edfb30d6f6cb6d3d99e8d9b844fc004374e5e
Group3BCS1 <group3bcs1@gmail.com>
2021-04-22 17:47:12 +0300

    Exercise 3 modified

--- src/chapter5/exercise3.py
+++ src/chapter5/exercise3.py
@@ -12,4 +12,21 @@ def vending():
     print("Welcome to our vending machine change maker program \nChange maker initialized.")
 
 
-    
+    def Display():
+        """This function displays the stock to the user"""
+        for stock in stocks:
+            print(stock.get("number"), stock.get("stock"))
+
+
+    def select_stock():
+        """This function prompts the user to select stock and also quit
+            This exits the program when "q" is entered
+            if else its converts the input to float
+        """
+        select = input("Enter the purchase price (xx.xx) or `q' to quit: ")
+
+
+
+ if __name__ == '__main__':
+    vending()
+   
