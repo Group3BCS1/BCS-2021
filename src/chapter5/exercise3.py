fc6ab70722e6bdd4215557483b30f332fb382fc7
Group3BCS1 <group3bcs1@gmail.com>
2021-04-23 18:40:36 +0300

    working on deposits

--- src/chapter5/exercise3.py
+++ src/chapter5/exercise3.py
@@ -1,10 +1,12 @@
-#GROUP3BCS1
-#1. ATWANZIRE TIMOTHY IAN 2020/BCS/026/PS
-#2. AHWERA ATENSA 	2020/BCS/014/PS
-#3. MULIZA TYRONE	2020/BCS/074/PS
-#4. OKELLO JOSHUA	2020/BCS/006
+# Group 3 members
+# ATWANZIRE TIMOTHY IAN 2020/BCS/026/PS
+# AHWERA ATENSA 2020/BCS/014/PS
+# OKELLO JOSHUA 2020/BCS/006
+# MULIZA TYRONE 2020/BCS/074/PS
+
+# main function
 def vending():
-    """This function contains a dictionary and a list to store th"""
+    """This function contains a dictionary and a list to store the stock"""
     a = {"stock": "Nickels", "number": 25}
     b = {"stock": "Dimes", "number": 25}
     c = {"stock": "Quarters", "number": 25}
@@ -13,81 +15,71 @@ def vending():
 
     stocks = [a, b, c, d, e]
 
-    print("================================= RESTART =======================================")
+    print("================================= START =======================================")
     print("Welcome to our vending machine change maker program \nChange maker initialized.")
 
-
+    # Display Function
     def Display():
         """This function displays the stock to the user"""
+        # For loop
         for stock in stocks:
             print(stock.get("number"), stock.get("stock"))
 
 
-    def select_stock():
-        """This function prompts the user to select stock and also quit
-            This exits the program when "q" is entered
-            if else its converts the input to float
-        """
-        select = input("Enter the purchase price (xx.xx) or `q' to quit: ")
-
-        # This exception is to check for value errors
-        try:
-            if select == 'q':
-                exit()
-            else:
-                select = float(select)
-                return select
-        except:
-            print(f'Illegal price: Must be a non-negative multiple of 5 cents.')
-
-
-    def select_deposit():
-        """This function prompts the user for disposit"""
-
-        select = input("Indicate your deposit: ")
-        return select
-
     # initialize constants
-    nickel_deposit = 0.05
-    dimes = 0.1
-    quarters = 0.25
-    one_dollar = 1
-    five_deposit = 5
+    n = 0.05
+    d = 0.1
+    q = 0.25
+    o = 1
+    f = 5
 
-    #initializing Display to the stock
+    def menu():
+        print("""
+        Deposit Menu
+        'n' - deposit a nickel
+        'd' - deposit a dime"
+        'q' - deposit a quarter
+        'o' - deposit a one dolla bill
+        'f' - deposit a five dolla bill
+        'c' - cancel the purchase
+        """)
+
+    def due_payments(select):
+        """Function to convert the user's entry to dollars and cents"""
+        one = select // 1  # Gives number of dollars
+        one = select % 1  # Gives number of cents
+        print(f"Payment due: {a:.0f} dollars and {b * 100:.0f} cents")
+
     Display()
+    # main while loop
 
     while True:
         try:
-            # a variable to hold the select function
-            s = select_stock()
+            # prompting the user for price
+            price_entry = input("Enter the purchase price (xx.xx) or `q' to quit: ")
+            if price_entry == "q":
+                print("Quiting...")
+                break
+            price_entry = float(price_entry)
+            price = price_entry
 
-            #making a check if the number entered is a non-negative or a multiple of 0.05
-            if s % 0.05 != 0 and s < 0:
-                print("Illegal price: Must be a non-negative multiple of 5 cents.")
-                select_stock()
+            # if statement to check the price entry
+            if (price_entry > 0) and ((price_entry*100) % 5) == 0:
+                menu()
+                due_payments(price_entry)
+                total_pay = 0.00
+                while price_entry > total_pay:
+                    dep = input("Indicate your deposit: ")
+                    if dep == 'c':
+                        print("Please take the change below", total_pay)  # Function change
+                        break
+
+
+
             else:
-                print("'n' - deposit a nickel")
-                print("'d' - deposit a dime")
-                print("'q' - deposit a quarter")
-                print("'o' - deposit a one dolla bill")
-                print("'f' - deposit a five dolla bill")
-                print("'c' - cancel the purchase")
-                
-                #initailizing the select deposit func
-                entry = select_deposit()
-                if entry =='n':
-                    bal = s-nickel_deposit
-                    print(bal)
-                if entry == 'd':
-                    bal = d-nickel_deposit
-                    print(bal)
-                if entry == 'q':
-                    bal(bal)
-
-
+                print("Illegal price: Must be a non-negative multiple of 5 cents.")
         except:
             print("Illegal price: Must be a non-negative multiple of 5 cents.")
 
-if __name__ == '__main__':
-    vending()
+
+vending()
