7d795ae9f0bae40d7a202255810aedcdff0cb298
Group3BCS1 <group3bcs1@gmail.com>
2021-05-04 13:47:03 +0300

    This is a simple version of my project

--- src/mid term/project.py
+++ src/mid term/project.py
@@ -1,3 +1,6 @@
+import itertools
+
+
 # main function
 def water_bill():
     print("==========================RESTART==========================")
@@ -5,30 +8,61 @@ def water_bill():
     code = code.lower()
     initial_reading = int(input("Enter beginning meter reading: "))
     final_reading = int(input("Enter Ending meter reading: "))
+    print(" ")
 
     def prompt_readings():
         """This function displays initial and final readings to the user"""
-        print("Beginning Meter Reading: ", initial_reading)
-        print("Ending Meter Reading: ", final_reading)
+        print(f"Beginning Meter Reading:  {initial_reading: 09}")
+        print(f"Ending Meter Reading: {final_reading: 09}")
 
     def reading():
         """Function to Compute gallons of water used"""
         if initial_reading < final_reading:
             gallons = final_reading - initial_reading
             prompt_readings()
-            gallons = gallons/10
-            print("Gallons of water used ", gallons)
+            gallons = gallons / 10
+            print(f"Gallons of water used {gallons}")
         else:
             if initial_reading > final_reading:
                 gallons = 1000000000 - initial_reading
-                gallons += final_reading
-                gallons = gallons/10
-                print("Gallons of water used ", gallons)
+                gallons = gallons + final_reading
+                gallons = gallons / 10
+                print(f"Gallons of water used {gallons}")
         return gallons
 
+    def units():
+        g = reading()
+        four_million = 4000000
+        ten_million = 10000000
+        if code == "r":
+            amount = 5.00 + (0.0005 * g)
+            print(f"Amount billed: $ {round(amount, 2)}")
+        else:
+            if code == "c":
+                if g <= four_million:
+                    amount = 1000
+                    print(f"Amount billed: $ {round(amount, 2)}")
+                else:
+                    if g > four_million:
+                        addition_units = g - four_million
+                        amount = 1000 + (0.00025 * addition_units)
+                        print(f"Amount billed: $ {round(amount, 2)}")
+            else:
+                if code == "i":
+                    if g <= four_million:
+                        amount = 1000
+                        print(f"Amount billed: $ {round(amount, 2)}")
+                    else:
+                        if four_million < g < ten_million:
+                            amount = 2000
+                            print(f"Amount billed: $ {round(amount, 2)}")
+                        else:
+                            if g > ten_million:
+                                addition_units = g - ten_million
+                                amount = 2000 + (addition_units * 0.00025)
+                                print(f"Amount billed: $ {round(amount, 2)}")
 
-
-    reading()
+    itertools.repeat(units(), 4)
 
 
 water_bill()
