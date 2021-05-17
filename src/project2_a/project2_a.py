fd62f884e8527801718928cde1b18825f6f1f20d
GroupBCS1 <group3bcs1@gmail.com>
2021-05-17 14:47:46 -0700

    Completed project 02 part A

--- src/project2_a/project2_a.py
+++ src/project2_a/project2_a.py
@@ -1,1 +1,15 @@
-handle = open("measles.txt")
+try:
+    handle = open("measles.txt", 'r')  # opening the file
+except:
+    exit()
+
+f_name = input("Enter file name: ")
+new_file = open(f_name, 'w')
+year = input("Enter Year: ")
+for line in handle:
+    # comparing the year inputs
+    if (year == line[88:92]) or (year == line[88:91]) or (year == line[88:90] or (year == line[88:89])):
+        new_file.write(line)  # writing data in the new
+    else:
+        if (year == 'all') or (year == 'ALL') or (year == ' '):
+            new_file.write(line)  # writing data in the new
