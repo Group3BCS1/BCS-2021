9703e7ac89c19813f73cb95a52b14ffe5b08ef8a
GroupBCS1 <group3bcs1@gmail.com>
2021-05-21 05:05:30 -0700

    Final version of project 2

--- src/project2_a/project2_a.py
+++ src/project2_a/project2_a.py
@@ -1,15 +1,18 @@
 try:
     handle = open("measles.txt", 'r')  # opening the file
 except:
-    exit()
+    exit("file can't open")
 
 f_name = input("Enter file name: ")
 new_file = open(f_name, 'w')
 year = input("Enter Year: ")
-for line in handle:
-    # comparing the year inputs
-    if (year == line[88:92]) or (year == line[88:91]) or (year == line[88:90] or (year == line[88:89])):
-        new_file.write(line)  # writing data in the new
-    else:
-        if (year == 'all') or (year == 'ALL') or (year == ' '):
+try:
+    for line in handle:
+        # comparing the year inputs
+        if (year == line[88:92]) or (year == line[88:91]) or (year == line[88:90] or (year == line[88:89])):
             new_file.write(line)  # writing data in the new
+        else:
+            if (year == 'all') or (year == 'ALL') or (year == ''):
+                new_file.write(line)  # writing data in the new
+except:
+    print('Enter valid inputs')
